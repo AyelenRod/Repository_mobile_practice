fdf0fb7f88e2ccb27bcd3cb8608b7c82c3dc0d9f
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-10-20 14:17:28 -0600

    Delete APP MOVIL directory

--- APP MOVIL/Practica-Examen/src/Main.kt
+++
@@ -1,8 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val libro = Libro("Romeo y Julieta", "William Shakespeare")
-    println("Estado inicial de la propiedad 'estaPrestado' : ${libro.estaPrestado}")
-    libro.prestarLibro()
-    println("Estado de la propiedad 'estaPrestado' después de llamar a prestarLibro(): ${libro.estaPrestado}")
-}
