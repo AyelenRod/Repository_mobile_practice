9b29d469a556951bf6676bdb2aea910252b0f526
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-10-20 14:18:14 -0600

    Delete HTML_APP WEB directory

--- HTML_APP WEB/main.js
+++
@@ -1,6 +0,0 @@
-import validar_campo_texto from "./scripts/validation";
-import "./scripts/format";
-
-function main(){
-
-}
--- HTML_APP WEB/scripts/validation.js
+++
@@ -1,7 +0,0 @@
-function validar_campo_texto(lenght, charset, text){
-    if(text.lenght >= lenght){
-        return false;
-    }else{
-        return true;
-    }
-}
